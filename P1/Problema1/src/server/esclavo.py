bd7de81281d3cd30dfa8309478a5bbddcca6df30
RenatoAtencio <renatoatencioaedo@gmail.com>
2025-05-07 19:55:36 -0400

    cosas que faltaron

--- P1/Problema1/src/server/esclavo.py
+++ P1/Problema1/src/server/esclavo.py
@@ -3,26 +3,36 @@ from uuid import uuid4
 import uvicorn
 import httpx
 from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+import os
+import json
+from typing import List
 
-API_BASE_URL = "http://localhost:5000"  
+load_dotenv(".env")
 
-puerto = 8000
-nombre_db = "test"
+puerto = int(os.getenv("SLAVEPORT"))
+arr_databases = (os.getenv("SLAVEDB")).split(",")
+API_BASE_URL = os.getenv("API_BASE_URL")
+
+print(arr_databases)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Código a ejecutar al inicio
     async with httpx.AsyncClient() as client:
         try:
             response = await client.post(
                 f"{API_BASE_URL}/entry",
-                params={"puerto": puerto, "db": nombre_db}
+                params={
+                    "puerto": puerto, 
+                    "databases": arr_databases
+                }
             )
+
             print("Respuesta del maestro:", response.json())
         except Exception as e:
             print("Error al conectar con el maestro:", e)
 
-    yield  # Aquí se ejecuta la aplicación
+    yield  
 
     # Esto se ejecuta cuando se termina la ejecucion
     async with httpx.AsyncClient() as client:
@@ -38,15 +48,86 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Nodo esclavo", lifespan=lifespan)
 
+def recuperarDatabase(nombre_db):
+    ruta_db = os.path.join("..", "..", "databases", nombre_db + ".json")
+    
+    try:
+        with open(ruta_db, "r", encoding="utf-8") as f:
+            contenido = json.load(f)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Base de datos no encontrada")
+    
+    f.close()
+    return contenido
+
 @app.get("/query")
-def busqueda(type: str, busqueda: str):
-    if type == "tipo_doc":
-        print(f"búsqueda tipo_doc: {busqueda}")
-    elif type == "titulo":
-        print(f"búsqueda título: {busqueda}")
+def busqueda(busqueda: str, tipo_busqueda: int):
+
+    if tipo_busqueda == 1:  # Por titulo
+
+        rsp = []
+
+        for database in arr_databases:
+            datos_database = recuperarDatabase(database)
+            arr_coincidencias = []
+
+            for elem in datos_database["datos"]:
+
+                palabras_titulo = set(str(elem["titulo"]).split())
+                palabras_busqueda = set(busqueda.split())
+
+                coincidencias = palabras_titulo & palabras_busqueda
+                conteo = len(coincidencias)
+
+                if conteo > 0:
+
+                    print(f"Se encontro coincidencia para {busqueda}")
+                    arr_coincidencias.append({
+                        "titulo" : elem["titulo"],
+                        "value": conteo
+                    })
+
+
+            if len(arr_coincidencias) > 0:
+                rsp.append({
+                    "database": database,
+                    "coincidencias": arr_coincidencias 
+                })
+
+        return(rsp)
+
+
+
+    elif tipo_busqueda == 2: # Por tipo de documento
         
-    return {"type": type, "busqueda": busqueda}
+        rsp = []
+
+        for database in arr_databases:
+
+            if database == busqueda:
+                datos_database = recuperarDatabase(database)
+                arr_coincidencias = []
+
+                for elem in datos_database["datos"]:
+                    palabras_titulo = set(str(elem["titulo"]).split())
+                    palabras_busqueda = set(busqueda.split())
+
+                    coincidencias = palabras_titulo & palabras_busqueda
+                    conteo = len(coincidencias)
+
+                    arr_coincidencias.append({
+                        "titulo" : elem["titulo"],
+                        "value": conteo
+                    })
+
+                rsp.append({
+                    "database": database,
+                    "coincidencias": arr_coincidencias 
+                })
+
+                return(rsp)
+    else:
+        return "None"
 
 if __name__ == "__main__":
-
     uvicorn.run("esclavo:app", host="localhost", port=puerto, reload=True)
--- P1/Problema1/src/server/maestro.py
+++ P1/Problema1/src/server/maestro.py
@@ -1,71 +1,137 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 import os
 import uvicorn
 from dotenv import load_dotenv
+import httpx
+import asyncio
+from typing import List, Annotated
 
 app = FastAPI(title="Nodo Maestro")
 distribucion = []
 
 @app.get("/status")
 def read_root():
-    return {"message": "Nodo Maestro funcionando"}
+    """
+        Permite ver si el maestro esta funcionando
+    """
+    return {"msg": "Nodo Maestro funcionando"}
 
 
 @app.get("/conections")
 def read_root():
-    return distribucion
+    """
+        Permite obtener la distribucion, es decir, los esclavos conectados con sus respectivos 
+        puertos y las databases asociadas
+    """
+    return {"ditribucion" : distribucion}
+
+async def realizarBusqueda(port: int, busqueda: str, tipo_busqueda: int):
+    """
+        Realiza la llamada al esclavo
+    """
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(
+                f"http://localhost:{port}/query",
+                params={
+                    "tipo_busqueda": tipo_busqueda,
+                    "busqueda": busqueda
+                }
+            )
+            return response.json()
+        except Exception as e:
+            return ("Error al realizar la busqueda en el esclavo:", e)
 
 
 @app.get("/query")
-def busqueda(type: str, busqueda: str):
+def busqueda(edad: int, tipo_busqueda: int, busqueda: str):
+    """
+        Permite realizar las consultas a los esclavos
 
-    if type == "tipo_doc":
+        Devuelve las respuestas ordenadas segun el ranking
+    """
+
+
+    if tipo_busqueda == 1: # Por titulo
+        
+        for nodo in distribucion:   # Broadcast
+            print(f"Realizando busqueda en nodo: {nodo["puerto"]}")
+            respuesta = asyncio.run(realizarBusqueda(nodo["puerto"], busqueda, tipo_busqueda))
+            rsp = {
+                "msg": f"respuesta del nodo {nodo["puerto"]}",
+                "busqueda": busqueda,
+                "respuesta" : respuesta # Tiene la database, y la lista de resultados-value
+            }
+        
+        print(rsp)
+            
+    elif tipo_busqueda == 2: # Por tipo de documento
+        respuestas = []
+        arr_busqueda_por_tipo_doc = busqueda.split(',')
+
         for nodo in distribucion:
-            if nodo["db_tipo_doc"] == type:
-                # Buscar en el nodo especifico
-                return {"response": "búsqueda por tipo_doc"}
+            for database in nodo["databases"]:
+                for busqueda_tipo_doc in arr_busqueda_por_tipo_doc: # Multicast
+                    
+                    if busqueda_tipo_doc == database:
+                        arr_busqueda_por_tipo_doc.remove(busqueda_tipo_doc)
 
-    elif type == "titulo":
-        for nodo in distribucion:
-            # Duscar en todos los nodos
-            print(f"búsqueda título: {busqueda}")
+                        print(f"Realizando busqueda en nodo: {nodo["puerto"]}")
+                        rsp = asyncio.run(realizarBusqueda(nodo["puerto"], busqueda_tipo_doc, tipo_busqueda))
+                        
+                        respuestas.append({
+                            "msg": f"respuesta del nodo {nodo["puerto"]}",
+                            "busqueda": busqueda,
+                            "respuesta" : rsp   # Tiene la database, y la lista de resultados-value
+                        })
 
-    return {"type": type, "busqueda": busqueda}
+        
+        print(respuestas)
 
+    # Parcear las respuestas segun el ranking
+
+    
+    
 
 @app.post("/entry")
-def ingresoEsclavo(puerto: int, db: str):
+def ingresoEsclavo(puerto: int, databases: Annotated[List[str] | None, Query()]):
+    """
+        Permite a un nodo ingresar a la distribucion
+        
+        Debe indicar su puerto(Int) y las Databases (string Arr) a las cuales que tiene acceso
+    """
 
     for nodo in distribucion:
         if nodo["puerto"] == puerto:
             return {"response": "puerto tomado"}
 
+    print(databases)
+
     distribucion.append({
         "puerto": puerto,
-        "db_tipo_doc": db,
+        "databases": databases,
     })
 
-    return{
-        "response": "conexion exitosa"
-    }
+    return{"Respuesta": "conexion exitosa"}
 
 
 @app.delete("/exit")
 def salidaEsclavo(puerto: int):
-    
+    """
+        Permite la salida de un esclavo de la distribucion 
+
+        Necesita el puerto del esclavo (mismo puerto con el cual se conecto a la distribucion)
+    """
     for nodo in distribucion:
         if nodo["puerto"] == puerto:
             distribucion.remove(nodo)
             break
 
-    return{
-        "response": "desconexion exitosa"
-    }
+    return{"respuesta": "desconexion exitosa"}
 
 if __name__ == "__main__":
 
     load_dotenv(".env")
-
     puerto = int(os.getenv("HOSTPORT"))
     url = os.getenv("URL")
 
